ac80c6dac9f89f0a57ff91ec7696f8526a0dde8a
Srijan Datta <srjdatta7@gmail.com>
2026-05-30 00:11:21 -0500

    doesn't track .next now

--- frontend/.next/dev/static/chunks/frontend_app_layout_tsx_0lu86yf._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0urkdkh._.css","static/chunks/0r9g_next_dist_0kmpk3~._.js"],
-    source: "dynamic"
-});
